c3d1e45e35c323a4e0d8158a625bb5e5eed13fc7
Kevyn Hoyos <kahoyos3@gmail.com>
2022-12-11 21:00:02 -0600

    hjkl

--- scripts/main.js
+++ scripts/main.js
@@ -33,15 +33,5 @@ function setUserName() {
   myButton.onclick = () => {
     setUserName();
   };
-
-  var nav = document.querySelector('nav');
-
-  window.addEventListener('scroll', function(){
-    if(window.pageYOffset > 100) {
-      nav.classList.add('bg-dark', 'shadow');
-    } else {
-      nav.classList.remove('bg-dark', 'shadow');
-    }
-  });
   
   
