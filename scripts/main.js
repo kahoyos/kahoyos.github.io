2ffd02aafcd6bace3aa91009ce8e124cf10ad568
Kevyn Hoyos <kahoyos3@gmail.com>
2022-12-13 00:16:00 -0600

    fdsa

--- scripts/main.js
+++ scripts/main.js
@@ -11,5 +11,39 @@ myImage.onclick = () => {
 
 let padding = document.querySelector("nav").offsetHeight + "px";
 document.body.style.paddingTop=padding;
+
+
+$(function(){
+  $("body").on("click", function(e){
+    //alert('JQUERY Working');
+    if($(".navbar-collapse").hasClass("show")==true)
+    {
+      $(".navbar-toggler")[0].click();
+    }
+  });
+})
+
+const showOnPx = 200;
+const backToTopButton = document.querySelector(".back-to-top")
+
+const scrollContainer = () => {
+  return document.documentElement || document.body;
+};
+
+document.addEventListener("scroll", () => {
+  if (scrollContainer().scrollTop > showOnPx) {
+    backToTopButton.classList.remove("hiddenbutton")
+  } else {
+    backToTopButton.classList.add("hiddenbutton")
+  }
+})
+
+const goToTop = () => {
+  document.body.scrollIntoView({
+    behavior: "smooth",
+  });
+};
+
+backToTopButton.addEventListener("click", goToTop)
   
   
